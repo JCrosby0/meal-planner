97b360ae89be236bcaa16b5951e332e8591a0a8c
JCrosby0 <jcrosby@internode.on.net>
2021-02-21 14:59:34 +0800

    Updates

---
+++ precache-manifest.1e0a39ff3d000fd413bc7259fbc754ff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca29b50078626d641e2b",
+    "url": "/sudoku-web-app/css/app.f82bf30e.css"
+  },
+  {
+    "revision": "206731eee439b927fbe8ca431df5aada",
+    "url": "/sudoku-web-app/img/bangers-mash.206731ee.jpg"
+  },
+  {
+    "revision": "a00f8c810aeb3ceedcf510299f289675",
+    "url": "/sudoku-web-app/img/beef-lasagne.a00f8c81.jpeg"
+  },
+  {
+    "revision": "07ac304cad50cd5aba48eeb1e5497d38",
+    "url": "/sudoku-web-app/img/butterflied-lamb.07ac304c.jpeg"
+  },
+  {
+    "revision": "b9abae06494b8019eeef690f5e91f3d6",
+    "url": "/sudoku-web-app/img/chicken-chorizo-risotto.b9abae06.jpeg"
+  },
+  {
+    "revision": "f4e2a9ead64fda2c1884508b8b4da40a",
+    "url": "/sudoku-web-app/img/chicken-pumpkin-risotto.f4e2a9ea.jpeg"
+  },
+  {
+    "revision": "4f4b32f9e803ddf3f297e086f709b171",
+    "url": "/sudoku-web-app/img/curried-sausages.4f4b32f9.jpeg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/sudoku-web-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0d78dba0b484239d4d9ce377446b5c95",
+    "url": "/sudoku-web-app/img/spaghetti-bolognese.0d78dba0.jpeg"
+  },
+  {
+    "revision": "88c8e93c4adf3575e2dbb621f0e4e9f2",
+    "url": "/sudoku-web-app/img/tacos.88c8e93c.jpg"
+  },
+  {
+    "revision": "adade08ef84d2bf57a8aa9c0bd20c097",
+    "url": "/sudoku-web-app/img/thai-green-chicken-curry.adade08e.jpeg"
+  },
+  {
+    "revision": "6e680af08cb5f49b6f5477d4339f6f88",
+    "url": "/sudoku-web-app/index.html"
+  },
+  {
+    "revision": "2201d6410d78567ac967",
+    "url": "/sudoku-web-app/js/about.e698aacd.js"
+  },
+  {
+    "revision": "ca29b50078626d641e2b",
+    "url": "/sudoku-web-app/js/app.5bcfacc0.js"
+  },
+  {
+    "revision": "547992ae27a5b96f7297",
+    "url": "/sudoku-web-app/js/chunk-vendors.f97cce3e.js"
+  },
+  {
+    "revision": "00bb71dea27824c9f49d5676a8c17d46",
+    "url": "/sudoku-web-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sudoku-web-app/robots.txt"
+  }
+]);
--- src/store/action-types.ts
+++
@@ -1,7 +0,0 @@
-export enum ActionTypes {
-  toggleMealId = "toggleMealId",
-  assignMeal = "assignMeal",
-  AddItemToShoppingList = "AddItemToShoppingList",
-  RemoveItemFromShoppingList = "RemoveItemFromShoppingList",
-  ToggleIngredientChecked = "ToggleIngredientChecked"
-}
